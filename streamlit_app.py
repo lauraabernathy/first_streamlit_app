2a7f43b71b30c7e4576e72e3b0c248abe6903d00
lauraabernathy <57771230+lauraabernathy@users.noreply.github.com>
2023-02-03 09:05:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+my_cur.execute("Select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
 streamlit.text(my_data_row)
 streamlit.title('My Parents New Healthy Diner')
 
@@ -36,10 +40,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for entering', add_my_fruit)
 
-my_cur.execute("Select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
+
 
 
 # write your own comment -what does the next line do? 
