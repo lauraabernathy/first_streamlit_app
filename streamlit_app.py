7dcc34ac2ce15235aef72b1968e02d2283c14ccc
lauraabernathy <57771230+lauraabernathy@users.noreply.github.com>
2023-02-03 09:31:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
 streamlit.write('Thanks for entering', add_my_fruit)
-my_cur_execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
