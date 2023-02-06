f2b181e0fad52a6b701341273577651d79b87c95
lauraabernathy <57771230+lauraabernathy@users.noreply.github.com>
2023-02-06 07:55:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,11 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+streamlit.dataframe(fruits_to_show)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
 streamlit.write('Thanks for entering', add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-streamlit.dataframe(fruits_to_show)
 
 
 ###
