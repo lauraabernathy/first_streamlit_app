6ff6f589df11a5ffe789614209fa5702fdd56237
lauraabernathy <57771230+lauraabernathy@users.noreply.github.com>
2023-02-06 06:46:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
-streamlit.write('The user entered ', fruit_choice)
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-fruitstreamlit.dataframe(fruityvice_normalized)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -40,7 +34,13 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 ###
-
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+streamlit.write('The user entered ', fruit_choice)
+#import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+fruitstreamlit.dataframe(fruityvice_normalized)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
